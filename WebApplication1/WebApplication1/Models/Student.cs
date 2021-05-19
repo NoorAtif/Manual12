6ac7172ba877a550539d0d5887b72fe7359e9d1f
NoorAtif129 <77112713+NoorAtif@users.noreply.github.com>
2021-05-19 17:48:13 +0500

    Initial Work

---
+++ WebApplication1/WebApplication1/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Student
+    {
+        public String Name { get; set; }
+        public String RegistrationNumber { get; set; }
+        public DateTime DOB { get; set; }
+
+    }
+}
